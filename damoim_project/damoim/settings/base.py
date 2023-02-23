77e6df33a0732d944f36c18bced77ff8a5eed2ec
황혁주 <nugulhie@gmail.com>
2023-02-23 22:37:28 +0900

    Add: Hexagonal Architecture 적용, swagger, middleware 추가

--- damoim_project/damoim/settings/base.py
+++ damoim_project/damoim/settings/base.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'libs.base.middleware.ReponseMiddleware'
 ]
 ROOT_URLCONF = "damoim.urls"
 
---
+++ damoim_project/user/domain/model/UserSet.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+@dataclass
+class UserData:
+    def get_uuid(self) -> str :
+        raise NotImplementedError()
+@dataclass
+class UserSet(UserData):
+    username:str
+    password:str
+    uuids:int
+
+    def get_uuid(self) -> str:
+        return self.uuids
+
